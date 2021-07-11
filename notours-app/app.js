c15ca539bce752ecb98334ecceeaab4d5f3016fa
Rajagopal P <rpallath@rmplc.net>
2021-07-11 22:22:35 +0530

    api implementation started with dummy data

--- notours-app/app.js
+++ notours-app/app.js
@@ -1,10 +1,22 @@
 import express from 'express';
+import fs from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const PORT = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello!', app: 'Notours!'});
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        results: tours.length,
+        data: {
+            tours: tours
+        }
+    });
 })
 
 app.listen(PORT, () => {
