8e8f0a075a32c491cee96af62611d07af148a3fb
Rajagopal P <rpallath@rmplc.net>
2021-07-20 16:34:03 +0530

    eslint and prettier integrated

--- notours-app/server.js
+++ notours-app/server.js
@@ -2,9 +2,10 @@
  * Everything not related to express will goes here
  */
 import dotenv from 'dotenv';
-dotenv.config({ path: './config.env' });
 
 import { app } from './app.js';
+
+dotenv.config({ path: './config.env' });
 
 const PORT = process.env.PORT || 3000;
 
