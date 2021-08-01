e2e30f396f6c88ca6ecceffdb3e18ee9a22b981d
Rajagopal P <rpallath@rmplc.net>
2021-08-02 00:33:12 +0530

    added virtual property in db schema

--- notours-app/models/tourModel.js
+++ notours-app/models/tourModel.js
@@ -1,61 +1,67 @@
 import mongoose from 'mongoose';
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    rating: {
+      type: Number,
+      default: 4.5,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      required: [true, 'A tour must have a summary'],
+      trim: true, // removes blank spaces from the end and beginning of the string.
+    },
+    description: {
+      type: String,
+      trim: true, // removes blank spaces from the end and beginning of the string.
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a image cover'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, // permanently hides this field from the output
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  rating: {
-    type: Number,
-    default: 4.5,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    required: [true, 'A tour must have a summary'],
-    trim: true, // removes blank spaces from the end and beginning of the string.
-  },
-  description: {
-    type: String,
-    trim: true, // removes blank spaces from the end and beginning of the string.
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a image cover'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false, // permanently hides this field from the output
-  },
-  startDates: [Date],
-});
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 /**
  * Virtual Property
